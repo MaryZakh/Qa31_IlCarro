143b9667912a1edde9ca8f5a49ac9faf075b3274
Maria Z <z0559882272@gmail.com>
2025-11-25 20:59:29 +0200

    added AddNewCarSuccess with photo

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -9,6 +9,7 @@ public class ApplicationManager {
 
 WebDriver wd;
 HelperUser helperUser;
+HelperCar helperCar;
 
      public void init() {
           wd = new ChromeDriver();
@@ -16,6 +17,7 @@ HelperUser helperUser;
           wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
           wd.navigate().to("https://ilcarro.web.app/");
           helperUser = new HelperUser(wd);
+          helperCar = new HelperCar(wd);
      }
 
      public void stop() {
@@ -24,5 +26,9 @@ HelperUser helperUser;
 
      public HelperUser getHelperUser() {
           return helperUser;
+     }
+
+     public HelperCar getHelperCar() {
+          return helperCar;
      }
 }
